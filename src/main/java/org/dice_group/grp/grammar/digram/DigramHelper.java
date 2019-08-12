30585f5a1ce789b01751d8fb182eb4d8fb894142
aalexandrams@gmail.com <aalexandrams@gmail.com>
2019-08-12 11:04:28 +0200

    new query for external nodes

--- src/main/java/org/dice_group/grp/grammar/digram/DigramHelper.java
+++ src/main/java/org/dice_group/grp/grammar/digram/DigramHelper.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -83,6 +84,11 @@ public class DigramHelper {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	public static Set<DigramOccurence> findDigrams(Model graph, Digram dig) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	/**
 	 * Searches the model for non overlapping occurrences, based on three main patterns:
@@ -107,7 +113,8 @@ public class DigramHelper {
 		Set<DigramOccurence> occurrences = new HashSet<DigramOccurence>();
 		
 		for(int i = 0; i< CASES.length; i++) {
-			List<QuerySolution>  results = queryModel(graph, CASES[i]);
+			QueryExecution queryExec = queryModel(graph, CASES[i]);
+			List<QuerySolution>  results = selectModel(queryExec);
 			for(QuerySolution solution: results){
 		        
 		        RDFNode n1 = solution.get("n1");
@@ -136,13 +143,21 @@ public class DigramHelper {
 				}
 				
 				if(!stmt1.equals(stmt2)) {
-					Set<RDFNode> externals = findExternals(stmt1, stmt2, graph);
+					List<RDFNode> nodes = new ArrayList<RDFNode>();
+					nodes.add(n1);
+					nodes.add(n2);
+					nodes.add(n3);
+					nodes.add(e1);
+					nodes.add(e2);
+					
+					Set<RDFNode> externals = findExternals(nodes, stmt1, stmt2, graph, CASES[i]);
 //					
 //			        Digram digram = new Digram(e1, e2, getExternalIndexes(stmt1, stmt2, externals));
 //			        digrams.add(digram);
 //			        
-			        //it's only an occurrence if it has at least one external node
-			        if(!externals.isEmpty()) {
+			        // it's only an occurrence if it has at least one external node
+					// maximum of 2 external nodes && externals.size() < 2
+			        if(!externals.isEmpty() ) {
 			        	DigramOccurence occurrence = new DigramOccurence(stmt1, stmt2, externals);
 			        	if(occurrences.isEmpty()) {
 			        		occurrences.add(occurrence);
@@ -163,73 +178,172 @@ public class DigramHelper {
 	 * @param stmt1
 	 * @param stmt2
 	 * @param graph
+	 * @param digramCase 
 	 * @return the set of external nodes
 	 */
-	public static Set<RDFNode> findExternals(Statement stmt1, Statement stmt2, Model graph){
+	public static Set<RDFNode> findExternals(List<RDFNode> nodes, Statement stmt1, Statement stmt2, Model graph, String digramCase){
 		Set<RDFNode> externals = new HashSet<RDFNode>();
+		String n1 = nodes.get(0).toString();
+		String n2 = nodes.get(1).toString();
+		String n3 = nodes.get(2).toString();
+		String e1 = nodes.get(3).toString();
+		String e2 = nodes.get(4).toString();
 		
+		StringBuilder n1Query = new StringBuilder();
+		StringBuilder n2Query = new StringBuilder();
+		StringBuilder n3Query = new StringBuilder();
 		
-		StringBuilder stb = new StringBuilder("select ?n1 ?e1 ?n2 ?e2 ?n3 where { ");
-		stb.append("VALUES (?n1) { ( ");
-		stb.append("<" + stmt1.getSubject() + "> ) ");
-		if(stmt1.getObject().isResource()) {
-			stb.append("(<" + stmt1.getObject() + "> ) ");
-		} else {
-			stb.append("(\"" + stmt1.getObject() + "\" ) ");
-		}
-		stb.append("( <"+ stmt2.getSubject() +"> ) ");
-		if(stmt2.getObject().isResource()) {
-			stb.append("( <"+ stmt2.getObject()+"> )} ");
-		} else {
-			stb.append("( \""+ stmt2.getObject()+"\" )} ");
-		}
-		stb.append("{?n1 ?e1 ?n2 .} UNION {?n3 ?e2 ?n1 .}}");
-		
+		switch (digramCase) {
+			// n1 - e1 - n2 - e2 - n3
+			case CASE_1:
+				n3 = getNodeName(nodes.get(2));
 				
-		List<QuerySolution> results = queryModel(graph, stb.toString());
-		
-		for(QuerySolution solution: results){			
-			Statement s1 = null;
-			RDFNode curNode = solution.get("n1");
-			if(curNode.isResource()) {
-				curNode = curNode.asResource();
+				// n1 
+				n1Query.append("SELECT * WHERE { { <");
+				n1Query.append(n1).append("> ?p ?o ."); 
+						//MINUS {<").append(n1).append("> <").append(e1).append("> <").append(n2).append("> . }");
+				n1Query.append("} UNION { ?s ?p1 <").append(n1).append("> .}} LIMIT 2");
+			
+				//n2
+				n2Query.append("SELECT * WHERE { { <");
+				n2Query.append(n2).append("> ?p ?o . ");
+				//MINUS { <").append(n2).append("> <").append(e2).append("> ").append(n3).append(" . }");
+				n2Query.append("} UNION { ?s ?p1 <").append(n2).append("> . ");
+				//MINUS { <").append(n1).append("> <").append(e1).append("> <").append(n2).append("> . } ");
+				n2Query.append("}} LIMIT 2");
 				
-				RDFNode n2t = solution.get("n2");
-				RDFNode el1 = solution.get("e1");
-				if(n2t!=null && el1!=null) {
-					Property e1 = ResourceFactory.createProperty(el1.toString());
-					s1 = ResourceFactory.createStatement(curNode.asResource(), e1, n2t);
+				//n3
+				n3Query.append("SELECT * WHERE { {");
+				n3Query.append(n3).append(" ?p ?o .");
+				n3Query.append("} UNION { ?s ?p1 ").append(n3).append(". ");
+				//MINUS {<").append(n2).append("> <").append(e2).append("> ").append(n3).append(" . } "
+				n3Query.append("}} LIMIT 2");
+				
+				break;
+			// n1 - e1 - n2 - n3 - e2 -n2
+			case CASE_2:
+				n2 = getNodeName(nodes.get(1));
+				
+				// n1 
+				n1Query.append("SELECT * WHERE { { (<");
+				n1Query.append(n1).append("> ?p ?o) "); //MINUS {(<").append(n1).append("> <").append(e1).append("> ").append(n2).append(")}");
+				n1Query.append("} UNION { (?s ?p1 <").append(n1).append(">)}} LIMIT 2");
+				
+				// n2
+				n2Query.append("SELECT * WHERE { {(");
+				n2Query.append(n2).append(" ?p ?o) ");//MINUS {(<").append(n1).append("> <").append(e2).append("> <").append(n3).append("> )}");
+				n2Query.append("} UNION { (?s ?p1 ").append(n2).append(") MINUS {(<").append(n3).append("> <").append(e2).append("> ")
+				.append(n2).append(")} }} LIMIT 2");
+				
+				// n3
+				n3Query.append("SELECT * WHERE { { (<");
+				n3Query.append(n3).append("> ?p ?o)");
+				n3Query.append("} UNION { (?s ?p1 <").append(n3).append(">) ");//MINUS {(<").append(n3).append("> <").append(e2).append("> ").append(n2).append(")} )"
+				n3Query.append("}} LIMIT 2");
+								
+				break;
+			// n2 - e1 - n1 - n2 - e2 - n3
+			case CASE_3:
+				n1 = getNodeName(nodes.get(0));
+				n3 = getNodeName(nodes.get(2));
+				
+				// n1 
+				n1Query.append("SELECT * WHERE { { (");
+				n1Query.append(n1).append(" ?p ?o) ");
+				n1Query.append("} UNION { (?s ?p1 ").append(n1).append(") ");//MINUS {(<").append(n2).append("> <").append(e1).append("> ").append(n1).append(")} ")
+				n1Query.append("}} LIMIT 2");
+				
+				// n2
+				n2Query.append("SELECT * WHERE { { (<");
+				n2Query.append(n2).append("> ?p ?o) ");//MINUS {(<").append(n2).append("> <").append(e1).append("> ").append(n1).append(")}");
+				//n2Query.append(" MINUS {(<").append(n2).append("> <").append(e2).append("> ").append(n3).append(")};
+				n2Query.append("} UNION { (?s ?p1 <").append(n2).append(">) }} LIMIT 2");
+				
+				// n3
+				n3Query.append("SELECT * WHERE { {(");
+				n3Query.append(n3).append(" ?p ?o) ");
+				n3Query.append("} UNION { (?s ?p1 ").append(n3).append(") ");//MINUS {(").append(n3).append(" <").append(e2).append("> <").append(n2).append(">)} "
+				n3Query.append("}} LIMIT 2");
+				
+				break;
+
+			default:
+				break;
+		}
+		
+		Map<String, RDFNode> map = new HashMap<String, RDFNode>();
+		map.put(n1Query.toString(), nodes.get(0));
+		map.put(n2Query.toString(), nodes.get(1));
+		map.put(n3Query.toString(), nodes.get(2));
+		
+		Iterator<String> keyIterator = map.keySet().iterator();
+		while (keyIterator.hasNext()) {
+			String curQuery = keyIterator.next();
+			QueryExecution queryExecution = queryModel(graph, curQuery);
+			List<QuerySolution> results = selectModel(queryExecution);
+			for (QuerySolution result: results) {
+				RDFNode n1Acq = result.get("n1");
+				RDFNode n2Acq = result.get("n2");
+				RDFNode n3Acq = result.get("n3");
+				RDFNode e1Acq = result.get("e1");
+				RDFNode e2Acq = result.get("e2");
+				
+				// n1 e1 n2
+				if(n1Acq != null && e1Acq != null && n2Acq != null && 
+						n1Acq.toString().equals(n1) && e1Acq.toString().equals(e1) && n2Acq.toString().equals(n2)) {
+					continue;
 				}
-				
+				// n2 e2 n3
+				if(n2Acq != null && e2Acq != null && n3Acq != null &&
+						n2Acq.toString().equals(n2) && e2Acq.toString().equals(e2) && n3Acq.toString().equals(n3)) {
+					continue;
+				}
+				// n1 e2 n3
+				if(n1Acq != null && e2Acq != null && n3Acq != null &&
+						n1Acq.toString().equals(n1) && e2Acq.toString().equals(e2) && n3Acq.toString().equals(n3)) {
+					continue;
+				}
+				// n3 e2 n2
+				if(n3Acq != null && e2Acq != null && n2Acq != null &&
+						n3Acq.toString().equals(n3) && e2Acq.toString().equals(e2) && n2Acq.toString().equals(n2)) {
+					continue;
+				}
+				// n3 e2 n2
+				if(n3Acq != null && e2Acq != null && n2Acq != null &&
+						n3Acq.toString().equals(n3) && e2Acq.toString().equals(e2) && n2Acq.toString().equals(n2)) {
+					continue;
+				}
+				externals.add(map.get(curQuery));
 			}
-			RDFNode el2 = solution.get("e2");
-			RDFNode tn3 = solution.get("n3");
-			Statement s2 = null;
-			if(el2 != null && tn3!= null) {
-				Property e2 = ResourceFactory.createProperty(el2.toString());
-				s2 = ResourceFactory.createStatement(tn3.asResource(), e2, curNode);
-			}
-				
-			
-			if(stmt1.equals(s1) || stmt2.equals(s1) ||stmt1.equals(s2) || stmt2.equals(s2)){
-				continue;
-			} 
-			
-			externals.add(curNode);
-			
-		}		
+		}
 		return externals;
 	}
 	
+	
+	
+	private static String getNodeName(RDFNode node) {
+		String nodeStr = "";
+		if(node.isResource()) {
+			nodeStr = new StringBuilder("<").append(node.toString()).append(">").toString();
+		} else {
+			nodeStr = new StringBuilder("\"").append(node.toString()).append("\"").toString();
+		}
+		return nodeStr;
+	}
+	
 	/**
-	 * 
+	 * executes a sparql query for a given model
 	 * @param graph
 	 * @param sparqlQuery
 	 * @return
 	 */
-	public static List<QuerySolution> queryModel(Model graph, String sparqlQuery) {
+	public static QueryExecution queryModel(Model graph, String sparqlQuery) {
 		Query query = QueryFactory.create(sparqlQuery);
 		QueryExecution queryExecution = QueryExecutionFactory.create(query, graph);
+		return queryExecution;
+	}
+	
+	private static List<QuerySolution> selectModel (QueryExecution queryExecution) {
 		List<QuerySolution> querySolutionList = new ArrayList<QuerySolution>();
 	    ResultSet resultSet = queryExecution.execSelect();
 	    while(resultSet.hasNext()) {
@@ -237,6 +351,10 @@ public class DigramHelper {
 		}
 		queryExecution.close();	
 		return querySolutionList;
+	}
+	
+	public static boolean askModel(QueryExecution queryExecution) {
+		return queryExecution.execAsk();
 	}
 	
 	/**
@@ -278,12 +396,7 @@ public class DigramHelper {
 	public static Set<Digram> getDigrams (Map<Digram, Set<DigramOccurence>> map){
 		return map.keySet();
 	}
-	
-	
-	public static Set<DigramOccurence> findDigrams(Model graph, Digram dig) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
 
 	/**
 	 * Sort the digrams by most to least frequent
