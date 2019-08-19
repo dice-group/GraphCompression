ef17f2da1121dd5922a9ee6c29bcc8f2fe482f9c
TortugaAttack <felixconrads@googlemail.com>
2019-08-19 21:04:26 +0200

    lucid structure in indexer test

--- src/test/java/org/dice_group/grp/index/IndexerTest.java
+++ src/test/java/org/dice_group/grp/index/IndexerTest.java
@@ -23,8 +23,7 @@ import org.rdfhdt.hdt.options.HDTSpecification;
 
 public class IndexerTest {
 
-	@Test
-	public void indexedGraphTest() {
+	private Model createGraph() {
 		Model graph = ModelFactory.createDefaultModel();
 		
 		graph.add(ResourceFactory.createResource("http://exa.com/s/S1"), ResourceFactory.createProperty("http://exa.com/p/P1"), 
@@ -35,38 +34,59 @@ public class IndexerTest {
 				ResourceFactory.createPlainLiteral("test"));
 		graph.add(ResourceFactory.createResource("http://exa.com/o/O1"), ResourceFactory.createProperty("http://exa.com/p/P1"), 
 				ResourceFactory.createPlainLiteral("abc"));
+		return graph;
+	}
+	
+	@Test
+	public void indexedGraphTest() {
+		Model graph = createGraph();
 		Model graphCopy = ModelFactory.createDefaultModel();
+		//copy the graph for assertion later 
 		graphCopy.add(graph);
+		
 		System.out.println("Original Graph: ");
 		System.out.println(graph);
+		
+		//Create HDT Dict
 		TempDictionary dict = DictionaryFactory.createTempDictionary(new HDTSpecification());
 
+		//Create indexer and index graph
 		Indexer index = new URIBasedIndexer(dict);
 		Model indexed = index.indexGraph(graph);
 
 		System.out.println("#########################\nIndexed Graph:");
 		System.out.println(indexed);
-		/*
-		 * S1 - P1 -> O1
-		 * O1 - P2 -> test
-		 * S1 - P2 -> test
-		 * O1 - P1 -> abc
-		 * 
-		 * index:
-		 * 
-		 * :s1, :p2, :o2
-		 * :s1, :p1, :o1
-		 * :s2, :p1, :o1
-		 * :s2, :p2, :o3
-		 * 
-		 */
-		//TODO check if this checkouts somehow (SPOILER it does)
-		Set<String> nodes = new HashSet<String>();
-		for(Statement stmt : indexed.listStatements().toSet()){
-			nodes.add(stmt.getSubject().toString());
-			nodes.add(stmt.getObject().toString());
-			nodes.add(stmt.getPredicate().toString());
+
+		Set<String> nodes = getNodesOfGraph(indexed);
+		Map<String, Node> nodeToOrig = expandNodes(nodes, index);
+		//expand triples in indexed
+		indexed = expandGraph(nodeToOrig, indexed);
+		System.out.println("#################\nExpanded:\n");
+		System.out.println(indexed);
+		System.out.println(graphCopy);
+
+		//check if indexed graph without the original graph is empty
+		assertEquals(0, indexed.difference(graphCopy).size());
+		//check if they are isomorphic 
+		assertTrue(indexed.isIsomorphicWith(graphCopy));
+		// both tests true -> same Size and same Statements -> same graph
+	}
+
+	private Model expandGraph(Map<String, Node> nodeToOrig, Model indexed) {
+		for(Statement stmt : indexed.listStatements().toList()) {
+			Triple t = new Triple(nodeToOrig.get(stmt.getSubject().getURI()),
+					nodeToOrig.get(stmt.getPredicate().getURI()),
+					nodeToOrig.get(stmt.getObject().toString()));
+				
+					
+			indexed.getGraph().add(t);
+			
+			indexed.remove(stmt);
 		}
+		return indexed;
+	}
+
+	private Map<String, Node> expandNodes(Set<String> nodes, Indexer index) {
 		Map<String, Node> nodeToOrig = new HashMap<String, Node>();
 		for(String s : nodes) {
 			char pre = s.charAt(1);
@@ -84,23 +104,18 @@ public class IndexerTest {
 			nodeToOrig.put(s, n);
 			System.out.println("Node "+s+" ---> "+n);
 		}
-		//expand triples in indexed
-		for(Statement stmt : indexed.listStatements().toList()) {
-			Triple t = new Triple(nodeToOrig.get(stmt.getSubject().getURI()),
-					nodeToOrig.get(stmt.getPredicate().getURI()),
-					nodeToOrig.get(stmt.getObject().toString()));
-				
-					
-			indexed.getGraph().add(t);
-			
-			indexed.remove(stmt);
-		}
-		System.out.println("#################\nExpanded:\n");
-		System.out.println(indexed);
-		System.out.println(graphCopy);
+		return nodeToOrig;
+	}
 
-		assertEquals(0, indexed.difference(graphCopy).size());
-		assertTrue(indexed.isIsomorphicWith(graphCopy));
+	private Set<String> getNodesOfGraph(Model indexed) {
+
+		Set<String> nodes = new HashSet<String>();
+		for(Statement stmt : indexed.listStatements().toSet()){
+			nodes.add(stmt.getSubject().toString());
+			nodes.add(stmt.getObject().toString());
+			nodes.add(stmt.getPredicate().toString());
+		}
+		return nodes;
 	}
 	
 }
